9ce265458f3125fcc8bccdbe9027faa3676a74a2
frimoldi.eth <francisco.rimoldi@gmail.com>
2023-10-29 12:40:35 -0300

    Delete perennial-v2 directory

--- perennial-v2/packages/perennial-oracle/util/buildChainlinkRoundId.ts
+++
@@ -1,5 +0,0 @@
-import { BigNumber } from '@ethersproject/bignumber'
-
-export function buildChainlinkRoundId(phaseId: number, aggregatorRoundId: number): BigNumber {
-  return BigNumber.from(16).pow(16).mul(phaseId).add(aggregatorRoundId)
-}
